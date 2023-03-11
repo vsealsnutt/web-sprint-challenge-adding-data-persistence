aae1aef037c0ba50ea1e1f92a78654e80760376d
Vanessa Seals-Nutt <v.sealsnutt@gmail.com>
2023-03-11 16:11:39 -0500

    started the server in index.js

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // start your server here
+const server = require('./api/server');
+
+const port = process.env.PORT || 9000;
+
+server.listen(port, () => {
+    console.log(`Listening on port ${port}`)
+});
