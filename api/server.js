902793f79e442648db57290f708b0f4f1bd397bb
Vanessa Seals-Nutt <v.sealsnutt@gmail.com>
2023-03-11 16:19:49 -0500

    built out the server

--- api/server.js
+++ api/server.js
@@ -1,1 +1,21 @@
 // build your server here and require it from index.js
+const express = require('express');
+const projectsRouter = require('./project/router');
+const resourcesRouter = require('./resource/router');
+const tasksRouter = require('./task/router');
+
+const server = express();
+
+server.use(express.json());
+server.use('/api/projects', projectsRouter);
+server.use('/api/resources', resourcesRouter);
+server.use('/api/tasks', tasksRouter);
+
+server.use((err, req, res, next) => { //eslint-disable-line
+    res.status(500).json({
+        message: err.message,
+        stack: err.stack
+    });
+});
+
+module.exports = server;
