d8496ee800541df4b7888e2523b8e45ad31145f4
Vanessa Seals-Nutt <v.sealsnutt@gmail.com>
2023-03-12 12:37:35 -0400

    built out projects model and router

--- api/project/model.js
+++ api/project/model.js
@@ -1,13 +1,24 @@
 // build your `Project` model here
 const db = require('../../data/dbConfig');
 
-function getProjects() {
-    return db('projects');
+async function getProjects() {
+    const projects = await db('projects');
+    const results = [];
+    for (let i = 0; i < projects.length; i++) {
+        let resp = {
+            project_id: projects[i].project_id,
+            project_name: projects[i].project_name,
+            project_description: projects[i].project_description,
+            project_completed: projects[i].project_completed === 0 ? false : true
+        }
+        results.push(resp);
+    }
+    return results;
 }
 
 async function createProject(project) {
     const [project_id] = await db('projects').insert(project);
-    return getProjects().where({project_id}).first();
+    return getProjects().where('project_id', project_id).first();
 }
 
 module.exports = {
--- api/project/router.js
+++ api/project/router.js
@@ -1,1 +1,22 @@
 // build your `/api/projects` router here
+const express = require('express');
+const Project = require('./model');
+const router = express.Router();
+
+router.get('/', (req, res, next) => {
+    Project.getProjects()
+        .then(projects => {
+            res.status(200).json(projects);
+        })
+        .catch(next)
+}) 
+
+router.post('/', (req, res, next) => {
+    Project.createProject(req.body)
+        .then(project => {
+            res.status(201).json(project);
+        })
+        .catch(next)
+})
+
+module.exports = router;
