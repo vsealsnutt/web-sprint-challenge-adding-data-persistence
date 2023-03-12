581f12b237fc65dea2ffbf7511271b0a710a993b
Vanessa Seals-Nutt <v.sealsnutt@gmail.com>
2023-03-12 09:15:26 -0400

    started building project model

--- api/project/model.js
+++ api/project/model.js
@@ -1,1 +1,16 @@
 // build your `Project` model here
+const db = require('../../data/dbConfig');
+
+function getProjects() {
+    return db('projects');
+}
+
+async function createProject(project) {
+    const [project_id] = await db('projects').insert(project);
+    return getProjects().where({project_id}).first();
+}
+
+module.exports = {
+    getProjects,
+    createProject
+}
