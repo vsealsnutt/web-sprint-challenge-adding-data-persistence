2ef18ccd5159f810f0b85bbcc4e5fc1e052a5c7f
Vanessa Seals-Nutt <v.sealsnutt@gmail.com>
2023-03-12 09:12:47 -0400

    corrected project_completed and task_completed format

--- data/migrations/20230311212624_first-migration.js
+++ data/migrations/20230311212624_first-migration.js
@@ -8,7 +8,7 @@ exports.up = async function(knex) {
             table.increments('project_id')
             table.string('project_name').notNullable()
             table.string('project_description')
-            table.boolean('project_completed').defaultTo(false)
+            table.integer('project_completed').defaultTo(0)
         })
         .createTable('resources', table => {
             table.increments('resource_id')
@@ -19,7 +19,7 @@ exports.up = async function(knex) {
             table.increments('task_id')
             table.string('task_description').notNullable()
             table.string('task_notes')
-            table.boolean('task_completed').defaultTo(false)
+            table.integer('task_completed').defaultTo(0)
             table.integer('project_id')
                 .unsigned()
                 .notNullable()
