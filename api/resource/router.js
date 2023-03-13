113a4b4a3277f92a1f92a9092b7e4c6786871e5e
Vanessa Seals-Nutt <v.sealsnutt@gmail.com>
2023-03-13 10:09:17 -0400

    built resources router

--- api/resource/router.js
+++ api/resource/router.js
@@ -1,1 +1,23 @@
 // build your `/api/resources` router here
+const express = require('express');
+const Resource = require('./model');
+const router = express.Router();
+
+router.get('/', (req, res, next) => {
+    Resource.getResources()
+        .then(resource => {
+            res.status(200).json(resource);
+        })
+        .catch(next)
+})
+
+router.post('/', async (req, res, next) => {
+    try {
+        const newResource = await Resource.createResource(req.body);
+        res.status(201).json(newResource);
+    } catch(err) {
+        next(err);
+    }
+})
+
+module.exports = router;
