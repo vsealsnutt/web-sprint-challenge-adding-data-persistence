5c2d64c9f04453072b9da061e004f83cd4f2f81a
Vanessa Seals-Nutt <v.sealsnutt@gmail.com>
2023-03-13 10:20:39 -0400

    built the tasks model

--- api/task/model.js
+++ api/task/model.js
@@ -1,1 +1,34 @@
 // build your `Task` model here
+const db = require('../../data/dbConfig');
+
+async function getTasks() {
+    const tasks = await db('tasks as t')
+        .join('projects as p',' t.project_id', 'p.project_id')
+        .select('t.*', 'p.project_name', 'p.project_description') 
+    const results = [];
+    for (let i = 0; i < tasks.length; i++) {
+        let resp = {
+            task_id: tasks[i].project_id,
+            task_description: tasks[i].project_description,
+            task_completed: tasks[i].task_completed === 0 ? false : true,
+            project_name: tasks[i].project_name,
+            project_description: tasks[i].project_description
+        }
+        results.push(resp);
+    }
+    return results;
+}
+
+async function createTask(task) {
+    const [id] = await db('tasks').insert(task);
+    return db('tasks as t')
+        .join('projects as p', 't.project_id', 'p.project_id')
+        .where('task_id', id)
+        .select('t.*')
+        .first()
+}
+
+module.exports = {
+    getTasks,
+    createTask
+}
