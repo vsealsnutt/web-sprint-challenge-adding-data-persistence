4fd5d535c3320c4668209393326decf7f3158fb1
Vanessa Seals-Nutt <v.sealsnutt@gmail.com>
2023-03-13 10:03:38 -0400

    built resource model

--- api/resource/model.js
+++ api/resource/model.js
@@ -1,1 +1,16 @@
 // build your `Resource` model here
+const db = require('../../data/dbConfig');
+
+function getResources() {
+    return db('resources');
+}
+
+async function createResource(resource) {
+    const [id] = await db('resources').insert(resource);
+    return db('resources').where('resource_id', id).first();
+}
+
+module.exports = {
+    getResources,
+    createResource
+}
